6fe0eca4eee8c98650de4e09f1ade211a1f99f02
BBO <bdz_brahim@yahoo.fr>
2019-09-05 15:15:48 +0200

    Update plotter.h

--- include/plotter.h
+++ include/plotter.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <condition_variable>
 #include <mutex>
+#include <queue>
 
 using namespace std;
 #include <matplotpp/matplotpp.h>
@@ -15,21 +16,20 @@ inline void wait_on_enter()
   std::getline(std::cin, input);
 }
 
-class MP : public MatPlot
-{
+class Plotter : public MatPlot {
 protected:
-  static MP *instance;
+  static Plotter *instance;
 
 public:
-  MP();
-  MP (std::mutex *mutex, std::condition_variable *cond_var, bool *notified, bool *done);
-  ~MP();
+  Plotter();
+  Plotter (std::mutex *mutex, std::condition_variable *cond_var, bool *notified, bool *done, std::queue<double> *data);
+  virtual ~Plotter();
 
   int is_run = 1;
   void getNewData();
   void startPlotter(int argc, char *argv[]);
 
-  // wrapping GLUT to c++ with singleton design pater is based on http://paulsolt.com/blog/2009/07/openglglut-classes-oop-and-problems
+  // wrapping GLUT to c++ with singleton design pattern is based on http://paulsolt.com/blog/2009/07/openglglut-classes-oop-and-problems
   static void displayWrapper();
   static void reshapeWrapper(int w, int h);
   static void idleWrapper();
@@ -37,15 +37,16 @@ public:
   static void motionWrapper(int x, int y);
   static void passiveWrapper(int x, int y);
   static void keyboardWrapper(unsigned char key, int x, int y);
-  static void setInstance(MP *mp);
+  static void setInstance(Plotter *mp);
 
 private:
   void DISPLAY();
-  double t = 0; // time
-  dvec x, z;
+  std::vector<double> x, z;
 
   std::mutex *mutex_;
   std::condition_variable *cond_var_;
   bool *notified_, *done_;
+  std::queue<double> *data_;
 };
+
 #endif
