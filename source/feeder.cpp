ff004958a8c8d2631b1e78e7664d49efc42cab45
BBO <bdz_brahim@yahoo.fr>
2019-09-05 15:16:17 +0200

    Update feeder.cpp

--- source/feeder.cpp
+++ source/feeder.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 #include "feeder.h"
 
-void feed(void) {
-  std::cout << "Miam!\n" << std::endl;
-  return;
+Feeder::Feeder() : mutex_(0), cond_var_(0), notified_(0), done_(0) {}
+
+Feeder::Feeder(std::mutex *mutex, std::condition_variable *cond_var, bool *notified, bool *done, std::queue<double> *data)
+: mutex_(mutex), cond_var_(cond_var), notified_(notified), done_(done), data_(data) {
+
+  unsigned int seed = 0;//std::random_device{}()
+  gen_.seed(seed);
+  dist_ = std::uniform_real_distribution<double>(0.0,1.0);
+}
+
+Feeder::~Feeder() {}
+
+void Feeder::run() {
+
+  for (int i = 0; i < 1000; ++i) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    std::unique_lock<std::mutex> lock(*mutex_);
+    data_->push(dist_(gen_));
+    *(this->notified_) = true;
+    (this->cond_var_)->notify_one();
+  }
+
+  *(this->done_) = true;
+  (this->cond_var_)->notify_one();
 }
