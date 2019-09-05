ff2f12574be6aa1916da3d3066b61b1b648ab230
BBO <bdz_brahim@yahoo.fr>
2019-09-05 15:17:12 +0200

    Update plotter.cpp

--- source/plotter.cpp
+++ source/plotter.cpp
@@ -1,31 +1,27 @@
 #include "plotter.h"
 
-MP *MP::instance = 0;
+Plotter *Plotter::instance = 0;
 
-MP::MP() : mutex_(0), cond_var_(0), notified_(0), done_(0) {}
+Plotter::Plotter() : mutex_(0), cond_var_(0), notified_(0), done_(0), data_(0) {}
 
-MP::MP(std::mutex *mutex, std::condition_variable *cond_var, bool *notified, bool *done)
-    : mutex_(mutex), cond_var_(cond_var), notified_(notified), done_(done) {}
+Plotter::Plotter(std::mutex *mutex, std::condition_variable *cond_var, bool *notified, bool *done, std::queue<double> *data)
+    : mutex_(mutex), cond_var_(cond_var), notified_(notified), done_(done), data_(data) {}
 
-MP::~MP() {}
+Plotter::~Plotter() {}
 
-void MP::setInstance(MP *mp)
-{
+void Plotter::setInstance(Plotter *mp) {
   instance = mp;
 }
 
-void MP::displayWrapper()
-{
+void Plotter::displayWrapper() {
   instance->display();
 }
 
-void MP::reshapeWrapper(int w, int h)
-{
+void Plotter::reshapeWrapper(int w, int h) {
   instance->reshape(w, h);
 }
 
-void MP::idleWrapper()
-{
+void Plotter::idleWrapper() {
   glutPostRedisplay();
 
   std::unique_lock<std::mutex> lock((*instance->mutex_));
@@ -37,48 +33,41 @@ void MP::idleWrapper()
   instance->getNewData();
   *(instance->notified_) = false;
 
-  if( *(instance->done_) ) {
+  if (*(instance->done_))
+  {
     glutDestroyWindow(glutGetWindow());
   }
 }
 
-void MP::mouseWrapper(int button, int state, int x, int y)
-{
+void Plotter::mouseWrapper(int button, int state, int x, int y) {
   instance->mouse(button, state, x, y);
 }
 
-void MP::motionWrapper(int x, int y)
-{
+void Plotter::motionWrapper(int x, int y) {
   instance->motion(x, y);
 }
 
-void MP::passiveWrapper(int x, int y)
-{
+void Plotter::passiveWrapper(int x, int y) {
   instance->passivemotion(x, y);
 }
 
-void MP::keyboardWrapper(unsigned char key, int x, int y)
-{
+void Plotter::keyboardWrapper(unsigned char key, int x, int y) {
   instance->keyboard(key, x, y);
-  if (key == 'r')
-  {
-    if (instance->is_run == 0)
-    {
+  if (key == 'r') {
+    if (instance->is_run == 0) {
       instance->is_run = 1;
-    }
-    else
-    {
+    } else {
       instance->is_run = 0;
     }
   }
 }
 
-void MP::startPlotter(int argc, char *argv[])
-{
+void Plotter::startPlotter(int argc, char *argv[]) {
 
   // Initialize GLUT
   glutInit(&argc, argv);
-  glutCreateWindow(100, 100, 400, 300);
+  glutCreateWindow(100, 100, 700, 600);
+  glutSetWindowTitle("c++ real time plot");
 
   // Function callbacks
   glutDisplayFunc(displayWrapper);
@@ -93,29 +82,21 @@ void MP::startPlotter(int argc, char *argv[])
   glutMainLoop();            // Start the main GLUT thread
 }
 
-void MP::DISPLAY()
-{
+void Plotter::DISPLAY() {
   //plot
   title("press r to run and stop");
   plot(x, z);
+  xlabel("x - samples");
+  ylabel("y - random noise");
+  grid(1);
 }
 
-void MP::getNewData()
-{
-
-  if (is_run)
-  {
-
-    t += 0.02;
-    x.push_back(x.size() + 1);
-    z.push_back(sin(3 * x.back() + t));
-
-    // int n = 10;
-    // for (int i = 0; i < n; ++i)
-    // {
-    //   t += 0.02;
-    //   x.push_back(x.size() + 1);
-    //   z.push_back(sin(3 * x[i] + t));
-    // }
+void Plotter::getNewData() {
+  if (is_run) {
+    while (!data_->empty()) {
+      x.push_back(x.size() + 1);
+      z.push_back(data_->front());
+      data_->pop();
+    }
   }
 }
